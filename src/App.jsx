60cba68cf673dc81f95df27780535ef99a2a8473
prithvisaini1222-a11y <prithvisaini1222@gmail.com>
2026-04-23 14:22:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,20 +16,18 @@ function App() {
   const [students,setStudents] = useState(initialStudents)
 
   const updateScore = (index,newScore)=>{
-    const updated = students.map(s=>s.id === index+1 ? {...s,score:Number(newScore)}:s)
+    const updated = students.map((s,i)=>
+      i === index ? {...s, score:Number(newScore)} : s
+    )
 
     setStudents(updated)
   }
-
-
- 
-
 
   return (
     <div className='w-full flex flex-col items-center gap-10 p-20 h-full bg-gray-900'>
     <Heading/>
     <AddStudent students={students} setStudents={setStudents}/>
-    <StudentsSummary/>
+    <StudentsSummary students={students}/>   {/* 🔥 important */}
     <AllDetails students={students} updateScore={updateScore}/>
     </div>
   )
